2b5c866c8189973859e649a5df266d3034b35232
oladoja19 <dotunoladoja19@gmail.com>
2022-03-24 07:57:59 -0700

    second commit

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,15 +10,15 @@
 char *_strncat(char *dest, char *src, int n)
 {
 
-	int dest_len, i;
-	for (dest_len = 0; dest[dest_len] != '\0'; dest_len++)
-		;
+int dest_len, i;
+for (dest_len = 0; dest[dest_len] != '\0'; dest_len++)
+;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[dest_len + i] = src[i];
+for (i = 0; i < n && src[i] != '\0'; i++)
+	dest[dest_len + i] = src[i];
 
-	/*should end with a end of string char*/
-	dest[dest_len + i] = '\0';
+/*should end with a end of string char*/
+dest[dest_len + i] = '\0';
 
-	return (dest);
+return (dest);
 }
