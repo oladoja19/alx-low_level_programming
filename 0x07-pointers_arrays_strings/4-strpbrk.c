9dacf4d29bbe25b481a46a92883e858a72d102c6
oladoja19 <dotunoladoja19@gmail.com>
2022-03-28 13:22:28 -0700

    commit

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- *  _strpbrk - Search a string for any of a set of bytes.
- *  @s: string
- *  @accept: string to match
- *  Return: Pointer to the byte in `s` that matches one of the bytes in `accept`
- *  or NULL if no such byte is found.
- *  */
+ * _strpbrk - Search a string for any of a set of bytes.
+ * @s: string
+ * @accept: string to match
+ * Return: Pointer to the byte in `s` that matches one of the bytes in `accept`
+ * or NULL if no such byte is found. 
+ */
 
 char *_strpbrk(char *s, char *accept)
 {
