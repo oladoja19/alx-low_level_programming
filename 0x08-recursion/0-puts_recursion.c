7c0412738a69323e44cb4bddc5ec665937895763
oladoja19 <dotunoladoja19@gmail.com>
2022-03-31 14:26:25 -0700

    first commit

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,7 +3,7 @@
 /**
  * _puts_recursion - prints a string, followed by a new line
  * @s: pointer to the string
- * Return: void
+ * * Return: void
  */
 
 void _puts_recursion(char *s)
@@ -18,3 +18,4 @@ void _puts_recursion(char *s)
 		_puts_recursion(s + 1);
 	}
 }
+
