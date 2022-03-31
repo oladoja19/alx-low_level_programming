3a49e9a8f7affb33ebf0ef8a68166da0dd34f974
oladoja19 <dotunoladoja19@gmail.com>
2022-03-31 14:43:47 -0700

    second commit

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "nain.h"
+
+/**
+ * _print_rev_recursion - prints string in revers
+ * @s: pointer to string
+ * *Return: void
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s > '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
