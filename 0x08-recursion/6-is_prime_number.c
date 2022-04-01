8cb546eae39fd652bd84526119ce179ce8779d65
oladoja19 <dotunoladoja19@gmail.com>
2022-04-01 15:18:03 -0700

    commit

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -23,9 +23,10 @@ return (check_prime(n, 2));
 int check_prime(int n, int resp)
 {
 
-	if (resp >= n && n > 1))
+	if (resp >= n && n > 1)
 		return (1);
-else if (n % resp == 0 || n <= 1)	
-		return (0);
+else if (n % resp == 0 || n <= 1)
+	return (0);
+	else
+		return (check_prime(n, resp + 1));
 }
-	
