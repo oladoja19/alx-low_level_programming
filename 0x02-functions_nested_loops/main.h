143ce6deb5a52bcd925a79a1b03e2f6fadad4689
oladoja19 <dotunoladoja19@gmail.com>
2022-04-10 06:45:36 -0700

    commit

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,31 +1,23 @@
-#define MAIN_H
+#include <unistd.h>
 
-/* _putchar.c */
-int _putchar(char);
-
-/*10 x alphabet */
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ *  */
+int _putchar(char c);
+int _putchar(char c);
 void print_alphabet(void);
-
-/* 2-print_alphabet_x10.c */
 void print_alphabet_x10(void);
-
 int _islower(int c);
-
-int _isalpha(int n);
-
+int _isalpha(int c);
 int print_sign(int n);
-
-int _abs(int n);
-
-int print_last_digit(int n);
-
+int _abs(int);
+int print_last_digit(int);
 void jack_bauer(void);
-
 void times_table(void);
-
-int add(int a, int b);
-
+int add(int, int);
 void print_to_98(int n);
-
 void print_times_table(int n);
-© 2022 GitHub, Inc.
